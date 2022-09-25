824c5e0ebcfb99c7765ca55e3e512d414969b71a
João Chrisóstomo <joao.chrisostomo@gmail.com>
2022-09-25 00:10:28 -0300

    update piece and program

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -4,10 +4,10 @@ public class Piece {
     protected Position position;
     protected Board board;
 
-    //
+    //construtor da peça //posição incial será nula para informar que ainda não está no tabuleiro
     public Piece(Board board) {
         this.board = board;
-        position = null;
+        position = null; //opcional
     }
 
     //o tabuleiro será acessado pelo pacote tabuleiro e subslacces de peças
